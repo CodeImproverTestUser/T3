cd4ffab5bb9192a7196a9702936e3ae16fee4c26
adjuc <anders@codeimprover.com>
2021-03-16 16:45:45 +0100

    SumOf7 implemented

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace T3
             Console.WriteLine("Hello World!");
             CalculateClass c1 = new CalculateClass();
             Console.Write("Sum of 5:" + c1.SumOf5(1,2,3,4,5).ToString()+ "\n"); 
+            Console.Write("Sum of 7:" + c1.SumOf7(1,2,3,4,5,6,7).ToString()+ "\n"); 
         }
     }
 }
