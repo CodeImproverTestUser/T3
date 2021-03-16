dd979d8e6df14ce25b6df74c3b76fe852802241c
adjuc <anders@codeimprover.com>
2021-03-16 14:39:14 +0100

    Basic program

---
+++ CalculateClass.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace T3
+{
+    class CalculateClass
+    {
+        public int SumOf5(int v1, int v2, int v3, int v4, int v5)
+        {
+            return(v1+v2+v3+v4+v5);
+        } 
+
+    }    
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CalculateClass c1 = new CalculateClass();
+            Console.Write("Sum of 5:" + c1.SumOf5(1,2,3,4,5).ToString()+ "\n"); 
+        }
+    }
+}
