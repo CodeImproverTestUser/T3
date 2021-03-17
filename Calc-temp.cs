d0429b26934058e4b0e9c703b9eb78fff9c337ec
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-03-17 16:11:33 +0100

    Efter afinstlation af test

--- Calc-temp.cs
+++ Calc-temp.cs
@@ -20,5 +20,9 @@ namespace T3
         {
             return(v1+v2+v3+v4+v5+v6+v7+v8, v9);
         } 
+        public int SumOf9(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9)
+        {
+            return(v1+v2+v3+v4+v5+v6+v7+v8, v9);
+        } 
     }    
 }
