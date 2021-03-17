23e69592e4cc73dbd1bb8ea5b516879309ad7709
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-03-17 15:07:59 +0100

    Update Calculate2.cs

--- Calculate2.cs
+++ Calculate2.cs
@@ -22,11 +22,6 @@ namespace T3
          public int SumOf8(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8)
         {
             return(v1+v2+v3+v4+v5+v6+v7+v8);
-        } 
-             1
-             2
-                      3
-                      4
-                      5
+        }
     }    
 }
