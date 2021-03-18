1aae51b27669c7d12a71247e61b27954df975b50
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-03-18 07:29:43 +0100

    Calc 5 efter 17/3 21.00 deploy

---
+++ Calc5.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace T3
+{
+    class CalculateClass
+    {
+        public int SumOf5(int v1, int v2, int v3, int v4, int v5)
+        { //test
+            return(v1+v2+v3+v4+v5);
+        } 
+        public int SumOf7(int v1, int v2, int v3, int v4, int v5, int v6, int v7)
+        { // test
+            return(v1+v2+v3+v4+v5+v6+v7);
+        } 
+        public int SumOf8(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8)
+        {
+            return(v1+v2+v3+v4+v5+v6+v7+v8);
+        } 
+        public int SumOf9(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9)
+        {
+            return(v1+v2+v3+v4+v5+v6+v7+v8, v9);
+        } 
+        public int SumOf9(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9)
+        {
+            return(v1+v2+v3+v4+v5+v6+v7+v8, v9);
+        } 
+    }    
+}
