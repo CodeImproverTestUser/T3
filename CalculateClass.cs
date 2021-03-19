f3e3fe0f60247be53e518319be4b86e3648055ec
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-03-19 14:53:32 +0100

    Update CalculateClass.cs

--- CalculateClass.cs
+++ CalculateClass.cs
@@ -12,6 +12,11 @@ namespace T3
         {
             return(v1+v2+v3+v4+v5+v6+v7);
         } 
+         public int SumOf5(int v1, int v2, int v3, int v4, int v5)
+        {
+            return(v1+v2+v3+v4+v5);
+        } 
+        public int SumOf7(int v1, int v2, int v3, int v4, int v5, int v6, int v7)
 
     }    
 }
