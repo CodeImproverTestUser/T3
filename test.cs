3944a4a6493a3329a3a6c704e39aedf9ea74f822
CodeImproverTestUser <77671164+CodeImproverTestUser@users.noreply.github.com>
2021-03-23 14:02:08 +0100

    Update test.cs

--- test.cs
+++ test.cs
@@ -8,7 +8,7 @@ namespace T3
         {
             return(v1+v2+v3+v4+v5);
         } 
-        public int SumOf7(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9)
+        public int SumOf7(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9, int v10)
         {
             return(v1+v2+v3+v4+v5+v6+v7);
         } 
